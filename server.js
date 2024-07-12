ac7da5bb8f23be41962e127b428c4c979f58f55a
Jaeha Yi <jaehaha1@gmail.com>
2024-07-12 14:07:13 -0600

    started calendar and animation

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+// Serve static files from the 'public' directory
+app.use(express.static('public'));
+
+// Serve glTF models from the 'models' directory
+app.use('/models', express.static(path.join(__dirname, 'models')));
+
+// Serve your main HTML file
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
